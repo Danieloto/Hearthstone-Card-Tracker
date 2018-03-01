31d46d80a9faad451dd3defbbc16343ed053530b
ThomasParis <tj.paris@iastate.edu>
2018-02-28 18:36:17 -0600

    Editing Deck Test Class

--- Heartstone_Deck_Track/src/deck/Deck.java
+++ Heartstone_Deck_Track/src/deck/Deck.java
@@ -4,25 +4,27 @@ import java.awt.Image;
 
 public class Deck extends Card{
 
-	private Card[] deck; //object array of cards to make up deck.
+	private Card[] deck = new Card[30]; //object array of cards to make up deck.
 	int count;
-	private Image[] images;
+	private Image[] images = new Image[30];
 	
 	public Deck(){ //Creates Deck
 		count = 0;
+		
 	}
 	
 	public void addCard(Card card){
 		if(count < 30){ //Adds card if deck is not full
 			deck[count] = card;
-			images[count] = card.largepicture();
+			//images[count] = card.largepicture();
+			count++;
 		}
 	}
 	
 	public void removeCard(Card card){ //Removes specified card
 		for(int i = 0; i < 30; i++){
 			if(deck[i] == card){
-				for(int j = 0; j < 29; j++){ //Shifts cards back a spot 
+				for(int j = 0; i+j < 29; j++){ //Shifts cards back a spot 
 					deck[i+j] = deck[i+j+1]; //to remove selected card
 				}
 				deck[29] = null; //Sets last card to empty
@@ -46,5 +48,18 @@ public class Deck extends Card{
 			throw new java.lang.Error("Card not in deck");
 		}
 		return deck[loc];
+	}
+	
+	public int getCardLocation(Card card){
+		int loc = -1;
+		for(int i = 0; i < 30; i++){
+			if(deck[i] == card){
+				loc = i;
+			}
+		}
+		if(loc == -1){
+			throw new java.lang.Error("Card not in deck");
+		}
+		return loc;
 	}
 }
